d5ca422a0d6f0ee81bfec43a429177325ceffdf5
Miguel Vieira <152816803+MiguelOlivieira@users.noreply.github.com>
2025-11-11 16:01:15 -0300

    feat(db) = add foreign key user_id na tabela usermeal e coluna session_id na tabela user

---
+++ db/migrations/20251111165500_add-foreign-key-to-userMeal.ts
@@ -0,0 +1,17 @@
+import { table } from "console";
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.alterTable('userMeal', (table) => {
+        table.uuid('user_id').notNullable();
+        table.foreign('user_id').references('id').inTable('user')
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+     await knex.schema.alterTable('userMeal', (table) => {
+        table.dropColumn('user_id')
+    })
+}
